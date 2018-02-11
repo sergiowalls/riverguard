4adf70b7b4efe626e27c99d3e5de71da23817e6e
Jordi Casanovas <jordi.casanovas@fib.upc.edu>
2018-02-11 13:16:43 +0100

    show tags along with tweets, should send dm. next build

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -4,4 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  consumer_key: '"B4vSFikLR3fUf6yavsATgEn9Y"',
+  consumer_secret: '"LtulxuF9ew3PzY9bpkEdmU28xZm3XNMYev8Lit2ADBv3GREDI3"',
+  access_token_key: '"246248425-Tlzgqm3gKduShh0C5bwpvBuHHPimD15rdrsxnTge"',
+  access_token_secret: '"woMKYYOMK7ytNPLSjij5tVUvm7nnOu4wADW9eITmb2CtU"'
 })
